d30b7ba4dcbe61849ea2a341f74b2f102d4ad5cf
MaxNadeev <e-max@bk.ru>
2024-03-18 22:54:31 +0300

    added feedback block

--- scripts/script.js
+++ scripts/script.js
@@ -45,3 +45,40 @@ function checkRadio(elementId) {
     }
     scrollToFilters();
 }
+
+document.getElementById('submit').addEventListener('click', function() {
+    const name = document.getElementById('name').value;
+    const phone = document.getElementById('phone').value;
+    
+    if (name.trim() === '') {
+        console.log('Please fill out NAME!');
+        document.getElementById('name').style.border = '2px solid red'
+        return;
+    }
+    if (phone.trim() === '') {
+        console.log('Please fill out PHONE!');
+        document.getElementById('phone').style.border = '2px solid red'
+        return;
+    }
+     else {
+        console.log('Name:', name);
+        console.log('Phone:', phone);
+
+        document.getElementById('name').disabled = true;
+        document.getElementById('phone').disabled = true;
+        
+        document.getElementById('name').value = '';
+        document.getElementById('phone').value = '';
+
+        document.getElementById('hide-after-success').style.display = 'none';
+        document.getElementById('success').style.display = 'block';
+    };
+});
+
+document.getElementById('again').addEventListener('click', function() {
+    console.log('again click');
+    document.getElementById('name').disabled = false;
+    document.getElementById('phone').disabled = false;
+    document.getElementById('hide-after-success').style.display = 'block';
+    document.getElementById('success').style.display = 'none';
+})
